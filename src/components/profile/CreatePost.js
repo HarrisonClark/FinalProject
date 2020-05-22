70232f107404b5926c1d3aeff94cd313c91abbd9
Rebecca Zhou <rmz6yx@virginia.edu>
2020-05-22 12:55:28 -0400

    work on createpost page

--- src/components/profile/CreatePost.js
+++ src/components/profile/CreatePost.js
@@ -4,29 +4,39 @@ import { TextField, Button } from '@material-ui/core';
 const CreatePost = () => {
   const [post, setPost] = useState('');
 
-  const handleSubmit = (post) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(post);
+  };
 
   return (
-    <div>
-      <TextField
-        // fullWidth
-        multiline
-        variant="outlined"
-        size="small"
-        label="Enter post..."
-        value={post}
-        onChange={(e) => setPost(e.target.value)}
-      />
-      <Button
-        size="small"
-        variant="contained"
-        color="primary"
-        text-transform="none"
-        onClick={handleSubmit}
-      >
-        Post
-      </Button>
-    </div>
+    <form style={{ direction: 'flex-container' }} align="center">
+      <div>
+        <TextField
+          // fullWidth
+          multiline
+          variant="outlined"
+          size="small"
+          label="Enter post..."
+          value={post}
+          onChange={(e) => {
+            setPost(e.target.value);
+          }}
+        />
+        {/* </div>
+      <div> */}
+        <Button
+          style={{ marginLeft: '10px', marginTop: '9px' }}
+          size="small"
+          variant="contained"
+          color="primary"
+          text-transform="none"
+          onSubmit={handleSubmit}
+        >
+          Post
+        </Button>
+      </div>
+    </form>
   );
 };
 
--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Paper from '../Paper';
 import CreatePost from './CreatePost';
+import { Typography } from '@material-ui/core';
 
 const ProfilePage = ({ first, last, username }) => {
   const content = (
     <div>
-      {first} {last}
+      <Typography variant="h6" gutterBottom align="left" color="primary">
+        {first} {last}
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom align="left" color="primary">
+        Welcome {username}!{' '}
+      </Typography>
       <br />
-      Welcome {username}!{/* <CreatePost /> */}
+      <CreatePost />
     </div>
   );
   return <Paper content={content} />;
